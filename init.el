36258e006931981d78096a1454deb72744ddd832
Razzi Abuissa <razzi.abuissa@goodeggs.com>
2019-01-05 18:58:02 -0700

    Evil magit stuff

--- init.el
+++ init.el
@@ -17,6 +17,7 @@
 (straight-use-package 'eval-sexp-fu)
 (straight-use-package 'evil)
 (straight-use-package 'evil-commentary)
+(straight-use-package 'evil-magit)
 (straight-use-package 'evil-surround)
 (straight-use-package 'flow-minor-mode)
 (straight-use-package 'flycheck)
@@ -30,6 +31,7 @@
 (straight-use-package 'rjsx-mode)
 (straight-use-package 'smartparens)
 (straight-use-package 'use-package)
+
 (straight-use-package
  '(zerodark-theme :host github :repo "NicolasPetton/zerodark-theme"))
 
@@ -46,26 +48,51 @@
 ;;   (counsel-mode +1))
 
 (ivy-mode)
+(eldoc-mode)
 (evil-mode 1)
+(evil-magit-init)
 (evil-commentary-mode)
 (set-face-attribute 'default nil :height 180)
 (dumb-jump-mode)
 (recentf-mode)
+(tool-bar-mode -1)
 
 (use-package evil-surround)
-;; (global-evil-surround-mode)
+(global-evil-surround-mode)
+
+(use-package magit
+  :config
+  (add-to-list 'magit-no-confirm 'stage-all-changes)
+
+  (add-hook 'git-commit-mode-hook 'evil-insert-state)
+
+  (setq
+    magit-display-buffer-function #'magit-display-buffer-fullframe-status-v1
+    magit-push-current-set-remote-if-missing nil
+    magit-commit-show-diff nil
+    magit-status-sections-hook
+    '(magit-insert-merge-log
+      magit-insert-rebase-sequence
+      magit-insert-am-sequence
+      magit-insert-sequencer-sequence
+      magit-insert-bisect-output
+      magit-insert-bisect-rest
+      magit-insert-bisect-log
+      magit-insert-unstaged-changes
+      magit-insert-untracked-files
+      magit-insert-staged-changes
+      magit-insert-status-headers
+      magit-insert-stashes
+      magit-insert-unpulled-from-upstream
+      magit-insert-unpulled-from-pushremote
+      magit-insert-unpushed-to-upstream
+      magit-insert-unpushed-to-pushremote)))
 
 ; this should load automatically
 (use-package flow-js2-mode)
 
 (add-hook 'js2-mode-hook 'flycheck-mode)
-(razzi-associate-extension-mode "js" rjsx-mode)
-
-(tool-bar-mode -1)
-
-(defun edit-init ()
-  (interactive)
-  (find-file "~/.emacs.d/init.el"))
+(razzi-associate-extension-mode "js" 'rjsx-mode)
 
 (setq
  frame-title-format "%f"
@@ -83,39 +110,38 @@
 		    "bd" 'kill-buffer
 		    "en" 'flycheck-next-error
 		    "ep" 'flycheck-previous-error
-		    "hdf" 'describe-funciton
+		    "hdf" 'describe-function
 		    "wd" 'delete-window
+		    "wo" 'other-window
 		    "qr" 'restart-emacs
 		    "qq" 'save-buffers-kill-terminal
 		    "wk" 'evil-window-up
 		    "wj" 'evil-window-down
 		    "wm" 'delete-other-windows
-		    "fi" 'edit-init
+		    "fi" 'crux-find-user-init-file
 		    "ff" 'find-file
 		    "fr" 'crux-recentf-find-file
-		    "f SPC" 'copy-file-name-to-clipboard
+		    "f SPC" 'razzi-copy-file-name-to-clipboard
+		    "o" 'razzi-put-after
+		    "O" 'razzi-put-before
 		    "ESC" 'kill-this-buffer
 		    "SPC" 'execute-extended-command
-		    "TAB" 'previous-buffer)
+		    "TAB" 'razzi-previous-useful-buffer)
 
-(defun copy-file-name-to-clipboard ()
-  "Copy the current buffer file name to the clipboard."
-  (interactive)
-  (let ((filename (if (equal major-mode 'dired-mode)
-                      default-directory
-                    (buffer-file-name))))
-    (when filename
-      (kill-new filename)
-      (message "Copied buffer file name '%s' to the clipboard." filename))))
+(general-define-key :states 'insert
+		    "C-t" 'razzi-transpose-previous-chars
+		    "C-c a" 'razzi-abbrev-or-add-global-abbrev
+		    "M-s" 'razzi-exit-insert-and-save
+		    "M-RET" 'eval-defun)
 
-(general-define-key :states 'insert "C-t" 'razzi-transpose-previous-chars)
-
-; (general-define-key :modes
 (general-define-key :states 'normal
 		    "[ SPC" 'razzi-insert-newline-before
 		    "] SPC" 'razzi-insert-newline-after
+		    "D" 'razzi-kill-line-and-whitespace
 		    "g]" 'dumb-jump-go
 		    "gs" 'magit-status
+		    "M-s" 'save-buffer
+		    "M-r" 'raise-sexp
 		    "M-RET" 'eval-defun
 		    "<backtab>" 'crux-switch-to-previous-buffer)
 
@@ -123,6 +149,9 @@
   "E" 'forward-symbol
   "ae" 'whole-buffer
   "SPC" 'evil-inner-symbol)
+
+(general-define-key :states 'visual
+		    "s" 'evil-surround-region)
 
 (setq js2-mode-show-parse-errors nil)
 (setq js2-mode-show-strict-warnings nil)
