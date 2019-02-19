8f67e6a9c5fdb7fd3bd62e8cf6944550ba23b178
Razzi Abuissa <razzi.abuissa@goodeggs.com>
2019-02-19 13:57:52 -0800

    A couple days worth of edits

--- init.el
+++ init.el
@@ -12,6 +12,7 @@
   (load bootstrap-file nil 'nomessage))
 
 (setq
+ fill-column 100
  ring-bell-function 'ignore
  shell-file-name "/bin/bash"
  evil-cross-lines t
@@ -39,7 +40,7 @@
 (straight-use-package 'evil-magit)
 (straight-use-package 'evil-surround)
 (straight-use-package 'flow-minor-mode)
-(straight-use-package 'flycheck-flow)
+
 (straight-use-package 'flycheck)
 (straight-use-package 'flycheck-package)
 (straight-use-package 'flycheck-package)
@@ -49,7 +50,7 @@
 (straight-use-package 'js2-mode)
 (straight-use-package 'markdown-mode)
 (straight-use-package 'magit)
-(straight-use-package 'projectile)
+
 (straight-use-package 'restart-emacs)
 (straight-use-package 'ripgrep)
 (straight-use-package 'rjsx-mode)
@@ -58,17 +59,35 @@
 (straight-use-package 'string-inflection)
 (straight-use-package 'use-package)
 
+(use-package undo-tree
+  :config
+  (global-undo-tree-mode))
+
+(use-package projectile
+  :config (projectile-mode 1))
+
+(use-package flycheck-flow
+  :config
+  (add-hook 'javascript-mode-hook 'flycheck-mode))
+
 (straight-use-package
  '(zerodark-theme :host github :repo "NicolasPetton/zerodark-theme"))
 
 (straight-use-package
  '(razzi :host github :repo "razzius/razzi.el"))
 
+(use-package js-comint
+  :config
+  (setq js-comint-program-command "/Users/razzi/apps/pick-api/node_modules/.bin/babel-node"))
+
 (use-package evil-matchit
   :config
   (global-evil-matchit-mode 1))
 
 (use-package flycheck-flow)
+
+(use-package tern
+  :init (add-hook 'js2-mode-hook 'tern-mode))
 
 (use-package golden-ratio
   :config
@@ -96,6 +115,7 @@
 
 (ivy-mode)
 (eldoc-mode -1)
+(global-linum-mode)
 (evil-mode 1)
 (evil-magit-init)
 (evil-commentary-mode)
@@ -103,8 +123,10 @@
 (dumb-jump-mode)
 (recentf-mode)
 (tool-bar-mode -1)
+(column-number-mode)
 (global-auto-revert-mode 1)
 (global-hl-line-mode 1)
+(global-subword-mode)
 
 (use-package evil-surround :config
   (global-evil-surround-mode))
@@ -165,6 +187,7 @@
 (general-define-key :states 'normal
 		    :prefix "SPC"
 		    "bb" 'ivy-switch-buffer
+		    "bs" 'razzi-switch-to-scratch-buffer
 		    "bn" 'next-buffer
 		    "bp" 'razzi-previous-useful-buffer
 		    "bd" 'kill-buffer
@@ -180,13 +203,21 @@
 		    "qr" 'restart-emacs
 		    "qq" 'save-buffers-kill-terminal
 		    "sl" 'ivy-resume
+		    "ss" 'swiper
+		    "td" 'toggle-debug-on-error
 		    "wk" 'evil-window-up
 		    "w2" 'evil-window-vsplit
+		    "w-" 'evil-window-split
+		    "wl" 'evil-window-right
 		    "wj" 'evil-window-down
+		    "wk" 'evil-window-up
 		    "wm" 'delete-other-windows
 		    "fi" 'crux-find-user-init-file
 		    "ff" 'find-file
-		    "fr" 'crux-recentf-find-file
+		    "ft" (lambda () (interactive) (find-file (replace-regexp-in-string "index.js" "test.js" (buffer-file-name))))
+		    "fe" (lambda () (interactive) (find-file (replace-regexp-in-string "test.js" "index.js" (buffer-file-name))))
+		    "fp" 'razzi-copy-project-file-path
+		    "fr" 'counsel-recentf
 		    "f SPC" 'razzi-copy-file-name-to-clipboard
 		    "o" 'razzi-put-after
 		    "tg" 'golden-ratio-mode
@@ -196,12 +227,15 @@
 		    "TAB" 'crux-switch-to-previous-buffer)
 
 (general-define-key :states 'normal
+		    "<up>" 'evil-scroll-line-up
+		    "<down>" 'evil-scroll-line-down
 		    "-" 'razzi-transpose-next-line
-		    "_" 'razzi-transpose-previous-line
-		    "[ SPC" 'razzi-insert-newline-before
-		    "] SPC" 'razzi-insert-newline-after
+		    "0" 'evil-first-non-blank
 		    "C-c r" 'rjsx-rename-tag-at-point
 		    "C-g" 'razzi-evil-mc-quit-and-quit
+		    "[ SPC" 'razzi-insert-newline-before
+		    "] SPC" 'razzi-insert-newline-after
+		    "_" 'razzi-transpose-previous-line
 		    "c" (general-key-dispatch 'evil-change
 			  "ru" 'string-inflection-upcase
 			  "rs" 'string-inflection-underscore
@@ -211,29 +245,31 @@
 			  "c" 'magit-commit)
 		    "C" 'razzi-change-line
 		    "D" 'razzi-kill-line-and-whitespace
+		    "M-/" 'evil-commentary-line
+		    "M-RET" 'eval-defun
+		    "M-[" 'evil-backward-paragraph
+		    "M-]" 'evil-forward-paragraph
+		    "M-d" 'iedit-mode
+		    "M-f" 'evil-ex-search-forward
+		    "M-l" 'evil-visual-line
+		    "M-n" 'flycheck-next-error
+		    "M-p" 'flycheck-previous-error
+		    "M-r" 'raise-sexp
+		    "M-s" 'razzi-flycheck-and-save-buffer
+		    "M-u" 'razzi-update-current-package
+		    "M-w" 'kill-current-buffer
 		    "Q" 'razzi-replay-q-macro
+		    "g/" 'razzi-ivy-search-at-point
 		    "g]" 'dumb-jump-go
 		    "gb" 'magit-blame-addition
 		    "gs" 'magit-status
-		    "g/" 'razzi-ivy-search-at-point
-		    "M-l" 'evil-visual-line
-		    "M-f" 'evil-search-forward
-		    "M-[" 'evil-backward-paragraph
-		    "M-]" 'evil-forward-paragraph
-		    "M-n" 'flycheck-next-error
-		    "M-p" 'flycheck-previous-error
-		    "M-d" 'iedit-mode
-		    "M-w" 'kill-current-buffer
-		    "M-s" 'razzi-flycheck-and-save-buffer
-		    "M-/" 'evil-commentary-line
-		    "M-u" 'razzi-update-current-package
-		    "M-r" 'raise-sexp
-		    "M-RET" 'eval-defun
 		    "o" 'razzi-open-with-comma
+		    ;; "/" 'evil-ex-search-forward
 		    "<backtab>" 'razzi-previous-useful-buffer)
 
 (general-define-key :states 'insert
-		    "C-i" 'hippie-expand
+		    "<tab>" 'yas-expand
+		    "<C-i>" 'hippie-expand
 		    "C-l" 'sp-forward-slurp-sexp
 		    "C-t" 'razzi-transpose-previous-chars
 		    "C-c a" 'razzi-abbrev-or-add-global-abbrev
@@ -244,6 +280,8 @@
 		    "M-v" 'razzi-paste
 		    "M-t" 'transpose-words
 		    "M-RET" 'eval-defun)
+
+(define-key input-decode-map "\C-i" [C-i]) ; does this work?
 
 (evil-define-text-object whole-buffer (count &optional beginning end type)
   (evil-range 0 (point-max)))
@@ -273,6 +311,9 @@
 (setq js2-mode-show-strict-warnings nil)
 (setq js2-strict-missing-semi-warning nil)
 (setq js-indent-level 2)
+
+(setq ivy-re-builders-alist
+      '((t . ivy--regex-fuzzy)))
 
 (when (equal system-type 'darwin)
   (setq mac-option-modifier 'super)
@@ -310,8 +351,54 @@
       (unless (file-exists-p dir)
 	(make-directory dir)))))
 
+(defun cd-project-root ()
+  (cd (projectile-project-root)))
+
+(add-hook 'find-file-hook 'cd-project-root)
+
 (advice-add 'find-file :before 'razzi-make-parent-directories)
 
+;; feature: paste into search minibuffer
+;; currently does not work
+(define-key minibuffer-local-map (kbd "M-v") 'yank)
+;; (define-key evil-ex-completion-map "\C-r" #'evil-paste-from-register)
+(define-key evil-ex-completion-map (kbd "M-v") 'yank)
+; does this need to refer to evil-vars?
+(eval-after-load 'evil-vars '(define-key evil-ex-completion-map (kbd "M-v") 'isearch-yank-kill))
+
+
+(defun razzi/magit-push ()
+  (interactive)
+  (magit-run-git "push"))
+
+(magit-define-popup-action 'magit-push-popup ?p "Push current :D" 'razzi/magit-push)
 
 (use-package zerodark-theme
   :demand t)
+
+(use-package dired+
+  :config
+  (setq
+   dired-recursive-copies 'always
+   dired-recursive-deletes 'always
+
+   diredp-hide-details-initially-flag t)
+  (define-key dired-mode-map (kbd "c") 'find-file)
+  (define-key dired-mode-map (kbd ".") 'dired-up-directory)
+  (define-key dired-mode-map (kbd "C-h") 'dired-up-directory)
+  (evil-define-key 'normal dired-mode-map (kbd "gs") 'magit-status)
+
+  (add-hook 'dired-mode-hook 'dired-hide-details-mode))
+; isearch c-t not work
+; visual u makes lowercase - not good
+; ui for number of errors
+; visual V to select the current paragraph or eol other brace: $%
+; goto-last-change: Invalid function: undo-tree-node-p
+; spc-based keybindings in * buffers: spc esc does not close
+; search then n does not work...? seems to use a different buffer
+; normal c-a increase number
+; ripgrep file names take up whole screen
+; visual eval flash region
+; git push current with progress or async - editor hangs at the moment
+; gc uncomment removes end-of-line //
+; partial keybinding show options
