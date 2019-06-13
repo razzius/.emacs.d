3c3b56452096bbd5868a9e3a38bb76f4097aa79b
Razzi Abuissa <razzi.abuissa@goodeggs.com>
2019-06-13 09:50:02 -0700

    Web stuff

--- init.el
+++ init.el
@@ -4,9 +4,9 @@
       (bootstrap-version 5))
   (unless (file-exists-p bootstrap-file)
     (with-current-buffer
-	(url-retrieve-synchronously
-	 "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
-	 'silent 'inhibit-cookies)
+      (url-retrieve-synchronously
+        "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+        'silent 'inhibit-cookies)
       (goto-char (point-max))
       (eval-print-last-sexp)))
   (load bootstrap-file nil 'nomessage))
@@ -18,6 +18,7 @@
  evil-ex-substitute-global t
  evil-regexp-search nil
  save-abbrevs 'silently
+ indent-tabs-mode nil
  ivy-initial-inputs-alist nil
  evil-shift-width 2
  straight-use-package-by-default t
@@ -39,9 +40,7 @@
 (straight-use-package 'evil-magit)
 (straight-use-package 'evil-surround)
 (straight-use-package 'flow-minor-mode)
-(straight-use-package 'flycheck-flow)
 (straight-use-package 'flycheck)
-(straight-use-package 'flycheck-package)
 (straight-use-package 'flycheck-package)
 (straight-use-package 'general)
 (straight-use-package 'golden-ratio)
@@ -80,6 +79,8 @@
 ;;   :config
 ;;   (global-evil-mc-mode 1))
 
+(use-package web-mode)
+
 (use-package iedit)
 
 (use-package counsel
@@ -105,6 +106,10 @@
 (tool-bar-mode -1)
 (global-auto-revert-mode 1)
 (global-hl-line-mode 1)
+(global-subword-mode)
+(global-linum-mode)
+(global-whitespace-mode 1)
+(setq whitespace-style '(trailing tabs tab-mark))
 
 (use-package evil-surround :config
   (global-evil-surround-mode))
@@ -145,10 +150,13 @@
 
 (use-package yasnippet
   :config
-  (yas-minor-mode))
+  (yas-global-mode 1))
 
+(add-hook 'python-mode-hook 'flycheck-mode)
 (add-hook 'js2-mode-hook 'flycheck-mode)
+
 (razzi-associate-extension-mode "js" 'rjsx-mode)
+(razzi-associate-extension-mode "html" 'web-mode)
 
 (setq-default
  evil-symbol-word-search t)
@@ -171,8 +179,10 @@
 		    "el" 'flycheck-list-errors
 		    "en" 'flycheck-next-error
 		    "ep" 'flycheck-previous-error
+		    "ev" 'flycheck-verify-setup
 		    "hdf" 'describe-function
 		    "hdv" 'describe-variable
+		    "d" 'razzi-put-debugger
 		    "wd" 'delete-window
 		    "wo" 'other-window
 		    "/" 'counsel-rg
@@ -184,18 +194,32 @@
 		    "w2" 'evil-window-vsplit
 		    "wj" 'evil-window-down
 		    "wm" 'delete-other-windows
+		    "fc" 'write-file
+		    "fR" 'spacemacs/rename-current-buffer-file
 		    "fi" 'crux-find-user-init-file
+		    "fn" 'razzi-copy-file-name
+		    "fd" 'razzi-copy-file-dir
+		    "fD" 'crux-delete-buffer-and-file
+		    "fo" 'crux-open-with
+					; redundant v_v
+		    "fp" 'razzi-copy-project-file-path
+		    "f RET" 'razzi-copy-project-file-path
+
 		    "ff" 'find-file
+		    "ft" '(lambda () (interactive) (find-file ()))
 		    "fr" 'crux-recentf-find-file
 		    "f SPC" 'razzi-copy-file-name-to-clipboard
+		    "ig" 'razzi/gray
 		    "o" 'razzi-put-after
 		    "tg" 'golden-ratio-mode
 		    "O" 'razzi-put-before
 		    "ESC" 'kill-this-buffer
 		    "SPC" 'execute-extended-command
+		    "RET" 'razzi-split-after-comma
 		    "TAB" 'crux-switch-to-previous-buffer)
 
 (general-define-key :states 'normal
+		    "$" 'razzi-almost-end-of-line
 		    "-" 'razzi-transpose-next-line
 		    "_" 'razzi-transpose-previous-line
 		    "[ SPC" 'razzi-insert-newline-before
@@ -218,6 +242,7 @@
 		    "g/" 'razzi-ivy-search-at-point
 		    "M-l" 'evil-visual-line
 		    "M-f" 'evil-search-forward
+		    "M-`" '(lambda () (interactive)) ; todo something useful
 		    "M-[" 'evil-backward-paragraph
 		    "M-]" 'evil-forward-paragraph
 		    "M-n" 'flycheck-next-error
@@ -234,6 +259,7 @@
 
 (general-define-key :states 'insert
 		    "C-i" 'hippie-expand
+		    "C-SPC" 'yas-expand
 		    "C-l" 'sp-forward-slurp-sexp
 		    "C-t" 'razzi-transpose-previous-chars
 		    "C-c a" 'razzi-abbrev-or-add-global-abbrev
@@ -254,6 +280,7 @@
 		    "SPC" 'evil-inner-symbol)
 
 (general-define-key :states 'visual
+		    "$" 'razzi-almost-end-of-line
 		    "c" 'evil-change
 		    "s" 'evil-surround-region
 		    "il" 'razzi-mark-line-text
@@ -281,6 +308,13 @@
 (add-hook 'before-save-hook 'delete-trailing-whitespace)
 (add-hook 'focus-out-hook 'garbage-collect)
 
+(use-package typescript-mode
+  :config (setq typescript-indent-level 2))
+
+(use-package virtualenvwrapper)
+
+(use-package pipenv)
+
 (use-package hippie-exp
   :config
   (setq hippie-expand-try-functions-list
@@ -289,8 +323,8 @@
   (defun hippie-expand-substitute-string (arg)
     "Remove extra paren when expanding line in smartparens"
     (if (and smartparens-mode
-  	     (memq (razzi-char-at-point) '(?} ?\))))
-  	(delete-char 1)))
+	     (memq (razzi-char-at-point) '(?} ?\))))
+	(delete-char 1)))
 
   (advice-add 'hippie-expand :after 'hippie-expand-substitute-string))
 
@@ -312,6 +346,58 @@
 
 (advice-add 'find-file :before 'razzi-make-parent-directories)
 
+;; from magnars
+(defun spacemacs/rename-current-buffer-file ()
+  "Renames current buffer and file it is visiting."
+  (interactive)
+  (let* ((name (buffer-name))
+	 (filename (buffer-file-name)))
+    (if (not (and filename (file-exists-p filename)))
+	(error "Buffer '%s' is not visiting a file!" name)
+      (let* ((dir (file-name-directory filename))
+	     (new-name (read-file-name "New name: " dir)))
+	(cond ((get-buffer new-name)
+	       (error "A buffer named '%s' already exists!" new-name))
+	      (t
+	       (let ((dir (file-name-directory new-name)))
+		 (when (and (not (file-exists-p dir)) (yes-or-no-p (format "Create directory '%s'?" dir)))
+		   (make-directory dir t)))
+	       (rename-file filename new-name 1)
+	       (rename-buffer new-name)
+	       (set-visited-file-name new-name)
+	       (set-buffer-modified-p nil)
+	       (when (fboundp 'recentf-add-file)
+		 (recentf-add-file new-name)
+		 (recentf-remove-if-non-kept filename))
+	       (when (and (configuration-layer/package-usedp 'projectile)
+			  (projectile-project-p))
+		 (call-interactively #'projectile-invalidate-cache))
+	       (message "File '%s' successfully renamed to '%s'" name (file-name-nondirectory new-name))))))))
+
+(use-package better-defaults)
+
+(defun razzi/gray ()
+  (interactive)
+  (razzi-run-script-on-file "gray"))
 
 (use-package zerodark-theme
   :demand t)
+
+; abbrev populate current word
+; prevent prettier from popping up when it finds an error
+; 0 to start of text (swap ^)
+; uncomment clears out intermediate //
+
+;
+;;;###autoload
+;; (defun razzi-open-file (command)
+;;   (interactive)
+;;   (save-buffer)
+;;   (shell-command ("open" (buffer-file-name))))
+; cursor here evals NEXT line?!?
+
+(global-hl-line-mode -1)
+
+;; (provide 'razzi)
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
+(load-theme `tron t)
